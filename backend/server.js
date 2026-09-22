a50c89988fcc7f9eebdebf28ff90ec43e1779cab
haymonnsantos <haymonn.santos@ba.estudante.senai.br>
2026-09-22 17:04:18 +0000

    alterado de produto para biblioteca

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,26 @@ api.use(express.json())
 
 // Rota GET / que retorna o produto criado por você
 api.get('/', (req, res) => {
-    const produto = {
-        nome: 'Livro de Filosofia', 
-        preco: 50.00, 
-        categoria: 'Livros'
-    }
+    const biblioteca = [
+        {
+            nome: 'Livro de Filosofia', 
+            preco: 50.00, 
+            categoria: 'Humanas'
+        },
+        {
+            nome: 'Livro de História',
+            preco: 40.00,
+            categoria: 'Humanas'
+        },
+        {
+            nome: 'Livro de Matemática',
+            preco: 60.00,
+            categoria: 'Exatas'
+        }
+    ]
     
     // Resposta em formato JSON usando a variável correta (res)
-    res.status(200).json(produto)
+    res.status(200).json(biblioteca)
 })
 
 api.listen(3000, () => {
